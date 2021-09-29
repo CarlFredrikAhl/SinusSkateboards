e4ab276fcb1aac15ed880795a49529a54479e062
Carl Fredrik Ahl <carlfredrikahl@gmail.com>
2021-09-29 14:20:28 +0200

    Added wheels page

---
+++ SinusSkateboards/Pages/Wheels.cshtml.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SinusSkateboards.Database;
+using SinusSkateboards.Models;
+
+namespace SinusSkateboards.Pages
+{
+    public class WheelsModel : PageModel
+    {
+        private readonly AppDbContext database;
+
+        public List<ProductModel> Products { get; set; }
+
+        public WheelsModel(AppDbContext context)
+        {
+            database = context;
+        }
+
+        public void OnGet()
+        {
+            //Add the products to the database if there are no ones already
+            if (!database.Products.Any())
+            {
+                Products = new List<ProductModel>()
+                {
+                    new ProductModel("sinus-wheel-rocket.png", "Wheel (rocket)", "Four rocket skate wheels"
+                    , "White/Red", 20),
+                    new ProductModel("sinus-wheel-spinner.png", "Wheel (spinner)", "Four white skate wheels"
+                    , "White", 20),
+                    new ProductModel("sinus-wheel-wave.png", "Wheel (wave)", "Four wave skate wheels"
+                    , "White/Black", 20),
+                };
+
+                //foreach (var product in Products)
+                //{
+                //    database.Products.Add(product);
+                //}
+
+                //database.SaveChanges();
+            }
+        }
+    }
+}
