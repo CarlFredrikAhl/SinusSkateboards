b9b54dd4bb782fe599d87c79c2d828a3a39a0c3d
Carl Fredrik Ahl <carlfredrikahl@gmail.com>
2021-09-29 13:34:45 +0200

    Added hoodies page and fixed some layout stuff

---
+++ SinusSkateboards/Pages/Hoodies.cshtml.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SinusSkateboards.Database;
+using SinusSkateboards.Models;
+
+namespace SinusSkateboards.Pages
+{
+    public class HoodiesModel : PageModel
+    {
+        private readonly AppDbContext database;
+
+        public List<ProductModel> Products { get; set; }
+
+        public HoodiesModel(AppDbContext context)
+        {
+            database = context;
+        }
+
+        public void OnGet()
+        {
+            //Add the products to the database if there are no ones already
+            if (!database.Products.Any())
+            {
+                Products = new List<ProductModel>()
+                {
+                    new ProductModel("hoodie-ash.png", "Hoodie (ash)", "Grey skate hoodie"
+                    , "Grey", 35),
+                    new ProductModel("hoodie-fire.png", "Hoodie (fire)", "Red skate hoodie"
+                    , "Red", 35),
+                    new ProductModel("hoodie-green.png", "Hoodie (green)", "Green skate hoodie"
+                    , "Green", 35),
+                    new ProductModel("hoodie-ocean.png", "Hoodie (ocean)", "Blue skate hoodie"
+                    , "Blue", 35),
+                    new ProductModel("hoodie-purple.png", "Hoodie (purple)", "Purple skate hoodie"
+                    , "Purple", 35),
+                };
+
+                //foreach (var product in Products)
+                //{
+                //    database.Products.Add(product);
+                //}
+
+                //database.SaveChanges();
+            }
+        }
+    }
+}
