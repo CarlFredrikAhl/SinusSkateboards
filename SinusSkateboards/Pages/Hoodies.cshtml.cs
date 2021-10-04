3ffc18069b2a673ef390de0045468d3d3d450d17
Carl Fredrik Ahl <carlfredrikahl@gmail.com>
2021-10-04 13:35:22 +0200

    Add to cart works for hoodies

--- SinusSkateboards/Pages/Hoodies.cshtml.cs
+++ SinusSkateboards/Pages/Hoodies.cshtml.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 using SinusSkateboards.Database;
 using SinusSkateboards.Models;
 
@@ -14,6 +16,8 @@ namespace SinusSkateboards.Pages
         private readonly AppDbContext database;
 
         public List<Product> Products { get; set; }
+
+        public int ItemsInCart { get; set; }
 
         public HoodiesModel(AppDbContext context)
         {
@@ -50,6 +54,51 @@ namespace SinusSkateboards.Pages
             {
                 Products = database.Products.Where(product => product.Title.Contains("Hoodie")).ToList();
             }
+
+            //Check how many items in cart
+            ItemsInCart = 0;
+
+            List<Product> cookieProducts = new List<Product>();
+
+            string stringProducts = HttpContext.Session.GetString("cart_items");
+
+            //Cookie products exists in the cart already
+            if (stringProducts != null)
+            {
+                cookieProducts = JsonConvert.DeserializeObject<List<Product>>(stringProducts);
+            }
+
+            foreach (var product in cookieProducts)
+            {
+                ItemsInCart++;
+            }
+        }
+
+        //Method for adding item to the cart
+        //Didn't work naming it to "OnPostAddToCart" and using asp-page-handler="AddToCart"
+        public IActionResult OnPost(int productId)
+        {
+            //Clicked product
+            var product = database.Products.Where(product => product.ProductId == productId).FirstOrDefault();
+
+            //Save to session cookie
+            List<Product> cookieProducts = new List<Product>();
+
+            string stringProducts = HttpContext.Session.GetString("cart_items");
+
+            //Cookie products exists in the cart already
+            if (stringProducts != null)
+            {
+                cookieProducts = JsonConvert.DeserializeObject<List<Product>>(stringProducts);
+            }
+
+            cookieProducts.Add(product);
+
+            stringProducts = JsonConvert.SerializeObject(cookieProducts);
+
+            HttpContext.Session.SetString("cart_items", stringProducts);
+
+            return RedirectToPage("/Hoodies");
         }
     }
 }
