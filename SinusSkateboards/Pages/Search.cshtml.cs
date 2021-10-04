bd739d8a3f62422174e99f839b960a5fe1797845
Carl Fredrik Ahl <carlfredrikahl@gmail.com>
2021-10-04 11:45:02 +0200

    Added serach to index page and a search page that works

--- SinusSkateboards/Pages/Search.cshtml.cs
+++ SinusSkateboards/Pages/Search.cshtml.cs
@@ -4,17 +4,50 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SinusSkateboards.Models;
+using SinusSkateboards.Database;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace SinusSkateboards.Pages
 {
     public class SearchModel : PageModel
     {
-        [BindProperty]
         public string SearchString { get; set; }
-        
-        public void OnGet(string searchString)
-        {
 
+        public List<Product> Products { get; set; }
+
+        public int ItemsInCart { get; set; }
+
+        private readonly AppDbContext database;
+
+        public SearchModel(AppDbContext context)
+        {
+            database = context;
+        }
+        
+        public void OnGet(string search)
+        {
+            Products = new List<Product>();
+            Products = database.Products.Where(product => product.Title.ToUpper().Contains(search.ToUpper())).ToList();
+
+            //Check how many items in cart
+            ItemsInCart = 0;
+
+            List<Product> cookieProducts = new List<Product>();
+
+            string stringProducts = HttpContext.Session.GetString("cart_items");
+
+            //Cookie products exists in the cart already
+            if (stringProducts != null)
+            {
+                cookieProducts = JsonConvert.DeserializeObject<List<Product>>(stringProducts);
+            }
+
+            foreach (var product in cookieProducts)
+            {
+                ItemsInCart++;
+            }
         }
     }
 }
